edeac832d0123fe8a6be86691b441a9cb9110218
FTPyreNova024 <jdaniel.mc4@gmail.com>
2024-09-05 21:11:33 -0400

    created the italy flag, willl now try to make it the mexico flag

--- topics/Challenges/Flag/js/script.js
+++ topics/Challenges/Flag/js/script.js
@@ -17,112 +17,49 @@ function setup() {
 }
 
 /**
- * Draws a house and the immediate environment
+ * sets the color of the background and the flag
  */
 function draw() {
 
-    // The sky
-    background(150, 200, 250);
+    background(73, 125, 209);
 
-    drawCloud();
+    drawGreen();
 
-    drawGround();
+    drawWhite();
 
-    drawHouse();
+    drawRed();
 }
 
-//draws a cloud
-function drawCloud() {
+//draws the left side of the flag
+function drawGreen() {
 
-    // A cloud
+    // the green side of the flag
     push();
-    noStroke();
-    // Note: using a single number for a colour will be greyscale
-    // (As if you used the same number for R, G, and B)
-    // So this is white:
-    fill(255);
-    ellipse(100, 100, 100, 100);
-    ellipse(180, 80, 100, 100);
-    ellipse(160, 120, 60, 60);
-    ellipse(190, 130, 60, 60);
-    ellipse(220, 120, 60, 60);
+    fill(37, 110, 47)
+    noStroke()
+    rect(150, 250, 230, 550)
     pop();
 }
 
-//draws the ground
-function drawGround() {
+//draws the white middle part of the flag
+function drawWhite() {
 
-     // The ground
+     // The white section of the flag
      push();
+     fill(255, 255, 255);
      noStroke();
-     fill(200);
-     rect(0, 400, 640, 480);
+     rect(380, 250, 230, 550 );
      pop();
 }
 
-//draws the house
-function drawHouse() {
+//draws the red side of the flag
+function drawRed() {
 
-    drawHouseRoof();
-
-    drawHouseBody();
-
-    drawHouseWindow();
-
-    drawDoor();
-}
-
-//Draws the roof
-function drawHouseRoof() {
-
-    // The main body of the house
+    // The red section of the flag
     push();
+    fill(255, 0 ,0);
     noStroke();
-    fill(250, 250, 200);
-    rect(200, 240, 280, 180);
+    rect(610, 250, 230, 550)
     pop();
 }
 
-//Draws the body of the house
-function drawHouseBody() {
-
-    // The roof
-    push();
-    noStroke();
-    // You can also write colors in hex code in quote marks
-    fill("#dc143c");
-    triangle(180, 240, 340, 120, 500, 240);
-    pop();
-}
-
-//Draws the window of the house
-function drawHouseWindow() {
-
-    // A window
-    push();
-    // You can also write colour names from the CSS standard in quotes
-    // https://www.w3.org/wiki/CSS/Properties/color/keywords
-    stroke("deeppink");
-    strokeWeight(5);
-    fill("blanchedalmond");
-    rect(220, 260, 80, 80);
-    pop();
-}
-
-//Draws the door
-function drawDoor() {
-
-    // The door
-    push();
-    noStroke();
-    fill(0, 128, 0);
-    rect(320, 300, 80, 120);
-    pop();
-
-    // The doorknob
-    push();
-    noStroke();
-    fill(255, 215, 0);
-    ellipse(340, 360, 10, 10);
-    pop();
-}
