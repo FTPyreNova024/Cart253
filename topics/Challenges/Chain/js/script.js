54fcb1991836b6d3c192ac42a25fad4b916b39ad
FTPyreNova024 <jdaniel.mc4@gmail.com>
2024-09-05 22:41:28 -0400

    got lazy and didnt want to do 5 separate functions so just used a do... while and it worked

--- topics/Challenges/Chain/js/script.js
+++ topics/Challenges/Chain/js/script.js
@@ -20,5 +20,28 @@ function setup() {
  * Creates the main elements
  */
 function draw() {
-    background();
+    background(255, 242, 0);
+
+    /**
+     * The main chain
+     */
+
+let i = 0;
+let x = 0;
+
+do { 
+    i = i + 1;
+    drawChainLink();
+    x = x + 200;
+} while (i <= 5);
+
+function drawChainLink() {
+    push();
+    noFill();
+    stroke(0);
+    strokeWeight(40)
+    ellipse(500, x, 200, 300);
+    pop();
 }
+}
+
