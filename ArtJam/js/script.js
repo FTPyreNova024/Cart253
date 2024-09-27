e57b7fc530019a320eacf2d07318089093ef502f
FTPyreNova024 <jdaniel.mc4@gmail.com>
2024-09-26 23:22:34 -0400

    idk why its not working

--- ArtJam/js/script.js
+++ ArtJam/js/script.js
@@ -3,39 +3,23 @@
  * 
  * By Daniel Munoz C
  * 
- * A representation of somone leaving someone else after a long time.
+ * A representation of two things changing by their distance.
  */
 
 "use strict";
 
-
-/**
- * Creates the different objects
- */
-
-const him = {
-    x: undefined,
-    y: undefined,
+const thingOne = {
+    x: undefined, // will be mouseX
+    y: undefined, // will be mouseY
     size: 100,
-    fill: (255, 0, 0),
+    fill: (255, 106, 0),
     fills: {
         isWith: (191, 0, 44),
         without: (128, 0, 29),
         letGo: (255, 51, 0),
     }
-}
+};
 
-const her = {
-    x: undefined,
-    y: undefined,
-    size: 100,
-    fill: (255, 0, 106),
-    fills: {
-        isWith: (191, 0, 44),
-        without: (255, 87, 157),
-        forgot: (255, 227, 239),
-    }
-}
 /**
  * Create the canvas
  */
@@ -44,9 +28,19 @@ function setup() {
 }
 
 /**
- * Draws the scene
+ * Move the user circle, check for overlap, draw the two circles
  */
 function draw() {
     background(0, 0, 0);
 
+    // Move thingOne
+    thingOne.x = mouseX;
+    thingOne.y = mouseY;
+
+    // Draw the two things
+    push();
+    noStroke();
+    fill(thingOne.fill);
+    ellipse(thingOne.x, thingOne.y, thingOne.size);
+    pop();
 }
