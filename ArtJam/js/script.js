0a7f2a36e3c4d2d56eb558a9b1e8f9f0791520bf
FTPyreNova024 <jdaniel.mc4@gmail.com>
2024-09-26 21:30:31 -0400

    idk

---
+++ ArtJam/js/script.js
@@ -0,0 +1,37 @@
+/**
+ * Mr. Furious
+ * Pippin Barr
+ *
+ * A guy who becomes visibly furious!
+ */
+
+"use strict";
+
+
+/**
+ * Creates the different objects
+ */
+
+let shapeRect = {
+    locX: mousex,
+    lockY: mousey,
+    fill: {
+        r: 168,
+        g: 34,
+        b: 43,
+    }
+}
+/**
+ * Create the canvas
+ */
+function setup() {
+    createCanvas(1000, 1000);
+}
+
+/**
+ * Draws the scene
+ */
+function draw() {
+    background(0, 0, 0);
+
+}
